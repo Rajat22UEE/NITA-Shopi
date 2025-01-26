9877647379386d2adcc4203fdab9e361009b6363
Sudipkarmakar25 <153487702+Sudipkarmakar25@users.noreply.github.com>
2025-01-26 08:50:36 +0530

    checking

--- FrontEnd/src/pages/OldProduct.jsx
+++ FrontEnd/src/pages/OldProduct.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const OldProduct = () => {
   return (
     <div className="container mx-auto">
-      <h1 className="text-2xl font-bold">Old Product Page</h1>
+      <h1 className="text-2xl font-bold">Old Product Page check</h1>
     </div>
   );
 };
